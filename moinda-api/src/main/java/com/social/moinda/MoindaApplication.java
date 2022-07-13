1166cdbc75350e33ebbf8d569b0b28a14ebd2466
kyh <kyh3964@naver.com>
2022-07-13 17:31:05 +0900

    [#8] 테스트를 위해 JPA 설정 코드 위치 변경

--- moinda-api/src/main/java/com/social/moinda/MoindaApplication.java
+++ moinda-api/src/main/java/com/social/moinda/MoindaApplication.java
@@ -2,19 +2,8 @@ package com.social.moinda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@PropertySources({
-        @PropertySource("classpath:application-core.properties")
-})
-@EntityScan("com.social.moinda.core")
-@EnableJpaRepositories("com.social.moinda.core")
-@EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = "com.social.moinda")
 public class MoindaApplication {
     public static void main(String[] args) {
